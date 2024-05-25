1eb3d37c139ba699b3ddebb8e9ac398413fe752c
Kim Ji Yong <jykim00324@gmail.com>
2024-05-25 23:18:37 +0900

    fix image load problem

--- src/app/(home)/_components/Major.tsx
+++ src/app/(home)/_components/Major.tsx
@@ -10,10 +10,14 @@ export default function Major({ major, bgImg, link }: MajorProps) {
   return (
     <Link
       href={`/${link}`}
-      className={`relative flex justify-center items-center group w-1/2 bg-[url(/images/${bgImg}.jpg)] bg-center`}
+      className="relative flex justify-center items-center group w-1/2"
     >
+      <div
+        className="absolute inset-0 bg-center bg-cover"
+        style={{ backgroundImage: `url(/images/${bgImg}.jpg)` }}
+      />
       <div className="absolute inset-0 bg-black opacity-60 group-hover:opacity-80 duration-500" />
-      <div className="text-6xl text-white z-10  animate-fadeIn group-hover:scale-110 duration-500">
+      <div className="text-6xl text-white z-10 animate-fadeIn group-hover:scale-110 duration-500">
         {major}
       </div>
     </Link>
